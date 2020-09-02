2ce42cacd17c6f383cff79f4d18b9f0e982d4526
FrankBlack <fonyeabo12@gmail.com>
2020-09-02 00:00:53 -0500

    Fullstack file management setup

--- propertysearchreactapp/client/src/App.js
+++ propertysearchreactapp/client/src/App.js
@@ -1,4 +1,11 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Discover from "./pages/Discover";
+import About from "./pages/About";
+import Search from "./pages/Search";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import Wrapper from "./components/Wrapper";
 import logo from './logo.svg';
 import './App.css';
 
