68c3030be68b653d6471ed8dbd368784f38be979
medszilla26 <pablohdzjr@gmail.com>
2020-09-19 13:51:17 -0500

    added class name App container to App.js

--- react-rental-prop-search-app/client/src/App.js
+++ react-rental-prop-search-app/client/src/App.js
@@ -1,20 +1,18 @@
-import React, { Component } from "react";
+import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import Routes from "./components/routes";
 import NavBar from "./components/nav";
 import Footer from "./components/footer";
 
-class App extends Component {
-  render() {
-    return (
-      <div>
-        <NavBar />
-        <Routes />
-        <Footer />
-      </div>
-    );
-  }
+function App() {
+  return (
+    <div className="App container">
+      <NavBar />
+      <Routes />
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
