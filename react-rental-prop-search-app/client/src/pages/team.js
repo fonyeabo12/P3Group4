d3beaf536f207f67f0950aea06c6faa5d1efe742
medszilla26 <pablohdzjr@gmail.com>
2020-09-26 14:24:42 -0500

    added custom social buttons to Team page, fixed links from blue to white

--- react-rental-prop-search-app/client/src/pages/team.js
+++ react-rental-prop-search-app/client/src/pages/team.js
@@ -1,9 +1,10 @@
 import React, { Component } from "react";
+import pablo from "../Images/pablo.JPG";
+import brandon from "../Images/brandon.png";
+import nick from "../Images/nick.jpg";
+import chidi from "../Images/chidi.jpeg";
+import { Container, Card, CardDeck, Col, Image } from "react-bootstrap";
 import "./style.css";
-import pablo from "../../src/Images/pablo.JPG";
-import brandon from "../../src/Images/brandon.png";
-import nick from "../../src/Images/nick.jpg";
-import { Container, Card, CardDeck, Button, Col, Image } from "react-bootstrap";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 class Team extends Component {
@@ -24,26 +25,81 @@ class Team extends Component {
             <Image className="avatar" src={brandon} />
             <Card.Body>
               <h5 className="name">Brandon Jenkins</h5>
-              <h6 className="title">Front End/Designer</h6>
-              <Button className="mx-1 mb-0">
+              <h6 className="title">Title</h6>
+              <a
+                className="github-btn"
+                type="button"
+                href="https://github.com/BrandonEJenkins"
+              >
                 <FontAwesomeIcon icon={["fab", "github"]} />
-              </Button>
-              <Button className="mx-1 mb-0">
-                <FontAwesomeIcon icon={["fab", "linkedin"]} />
-              </Button>
+              </a>
+              <span className="linkedin-btn" type="button">
+                <a
+                  href="https://www.linkedin.com/in/brandon-e-jenkins/"
+                  target="blank"
+                >
+                  <FontAwesomeIcon icon={["fab", "linkedin-in"]} />
+                </a>
+              </span>
+              <span className="email-btn" type="button">
+                <a href="mailto: pablohdzjr@gmail.com">
+                  <FontAwesomeIcon icon={["fas", "envelope"]} />
+                </a>
+              </span>
             </Card.Body>
           </Col>
           <Col className="my-5 px-1 pb-5 text-center border-0">
             <Image className="avatar" src={nick} />
             <Card.Body>
               <h5 className="name">Nicholas Foreman</h5>
-              <h6 className="title">Front End/Designer</h6>
-              <Button className="mx-1 mb-0">
+              <h6 className="title">Title</h6>
+              <a
+                className="github-btn"
+                type="button"
+                href="https://github.com/nickforeman4"
+              >
                 <FontAwesomeIcon icon={["fab", "github"]} />
-              </Button>
-              <Button className="mx-1 mb-0">
-                <FontAwesomeIcon icon={["fab", "linkedin"]} />
-              </Button>
+              </a>
+              <span className="linkedin-btn" type="button">
+                <a
+                  href="https://www.linkedin.com/in/nickforeman/"
+                  target="blank"
+                >
+                  <FontAwesomeIcon icon={["fab", "linkedin-in"]} />
+                </a>
+              </span>
+              <span className="email-btn" type="button">
+                <a href="mailto: pablohdzjr@gmail.com">
+                  <FontAwesomeIcon icon={["fas", "envelope"]} />
+                </a>
+              </span>
+            </Card.Body>
+          </Col>
+          <Col className="my-5 px-1 pb-5 text-center border-0">
+            <Image className="avatar" src={chidi} />
+            <Card.Body>
+              <h5 className="name">Chidi Onyeabo</h5>
+              <h6 className="title">Title</h6>
+              <a
+                className="github-btn"
+                type="button"
+                href="https://github.com/fonyeabo12"
+              >
+                <FontAwesomeIcon icon={["fab", "github"]} />
+              </a>
+              <span className="linkedin-btn" type="button">
+                <a
+                  href="https://www.linkedin.com/in/franklin-onyeabo/"
+                  target="blank"
+                >
+                  <FontAwesomeIcon icon={["fab", "linkedin-in"]} />
+                </a>
+              </span>
+              <span className="email-btn" type="button">
+                <a href="mailto: pablohdzjr@gmail.com">
+                  <FontAwesomeIcon icon={["fas", "envelope"]} />
+                </a>
+              </span>
             </Card.Body>
           </Col>
           <Col className="my-5 px-1 pb-5 text-center border-0">
@@ -51,25 +107,26 @@ class Team extends Component {
             <Card.Body>
               <h5 className="name">Pablo Hernandez</h5>
               <h6 className="title">Front End/Designer</h6>
-              <Button className="mx-1 mb-0">
+              <a
+                className="github-btn"
+                type="button"
+                href="https://github.com/medszilla26"
+              >
                 <FontAwesomeIcon icon={["fab", "github"]} />
-              </Button>
-              <Button className="mx-1 mb-0">
-                <FontAwesomeIcon icon={["fab", "linkedin"]} />
-              </Button>
-            </Card.Body>
-          </Col>
-          <Col className="my-5 px-1 pb-5 text-center border-0">
-            <Image className="avatar" src={pablo} />
-            <Card.Body>
-              <h5 className="name">Pablo Hernandez</h5>
-              <h6 className="title">Front End/Designer</h6>
-              <Button className="mx-1 mb-0">
-                <FontAwesomeIcon icon={["fab", "github"]} />
-              </Button>
-              <Button className="mx-1 mb-0">
-                <FontAwesomeIcon icon={["fab", "linkedin"]} />
-              </Button>
+              </a>
+              <span className="linkedin-btn" type="button">
+                <a
+                  href="https://www.linkedin.com/in/pablohdzjr/"
+                  target="blank"
+                >
+                  <FontAwesomeIcon icon={["fab", "linkedin-in"]} />
+                </a>
+              </span>
+              <span className="email-btn" type="button">
+                <a href="mailto: pablohdzjr@gmail.com">
+                  <FontAwesomeIcon icon={["fas", "envelope"]} />
+                </a>
+              </span>
             </Card.Body>
           </Col>
         </CardDeck>
