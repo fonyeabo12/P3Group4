b5209170e4c4b44d1d90ae49090c77447abb5dd1
BrandonEJenkins <brandonejenkins@gmail.com>
2020-09-29 11:02:06 -0400

    deploy

--- react-rental-prop-search-app/client/src/pages/about.js
+++ react-rental-prop-search-app/client/src/pages/about.js
@@ -1,26 +1,70 @@
 import React, { Component } from "react";
-import { Container } from "react-bootstrap";
+import { Container, Row, Col } from "react-bootstrap";
 
 class About extends Component {
   render() {
     return (
       <Container>
+
         <div className="about">
-          <div className="about-heading">About Us</div>
-          <p>
-            <span className="about-text-styling">Property Search</span> allows
-            you to quickly find your next home by providing a friendly one stop
-            site for your renting needs.
-          </p>
-          <p>
-            We provide a living database that offers current homes and
-            apartments that are available on the market.
-          </p>
-          <p>
-            <span className="about-text-styling">Property Search</span> launched
-            in 2020 and is headquartered in Houston.
-          </p>
+
+          <Row className="mb-5">
+            <Col>
+              <div className="about-heading">About Us</div>
+                <p>
+                  <span className="about-text-styling">Property Search</span> allows
+                  you to quickly find your next home by providing a friendly one stop
+                  site for your renting needs.
+                </p>
+                <p>
+                  We provide a living database that offers current homes and
+                  apartments that are available on the market.
+                </p>
+                <p>
+                  <span className="about-text-styling">Property Search</span> launched
+                  in 2020 and is headquartered in Houston.
+                </p>
+            </Col>
+          </Row>
+
+          <Row>
+            <h2 className="about-text-styling m-3">Why Did We Create This Application?</h2>
+              <p className="m-3">
+                We created this service to solve <strong>three critical problems</strong> facing our country right now:
+              </p>
+          </Row>
+
+          <Row className="m-3">
+            <div class="card-deck">
+              <div class="card shadow-sm p-3 mb-5 bg-white rounded border-0">
+                <img class="card-img-top" src="..." alt="Card image cap"/>
+                <div class="card-body">
+                  <h5 class="card-title"><strong>Affordable Rent</strong></h5>
+                  <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                  <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                </div>
+              </div>
+              <div class="card shadow-sm p-3 mb-5 bg-white rounded border-0">
+                <img class="card-img-top" src="..." alt="Card image cap"/>
+                <div class="card-body">
+                  <h5 class="card-title">Card title</h5>
+                  <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+                  <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                </div>
+              </div>
+              <div class="card shadow-sm p-3 mb-5 bg-white rounded border-0">
+                <img class="card-img-top" src="..." alt="Card image cap"/>
+                <div class="card-body">
+                  <h5 class="card-title">Card title</h5>
+                  <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+                  <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                </div>
+              </div>
+            </div>
+          </Row>
+
         </div>
+
       </Container>
     );
   }
--- react-rental-prop-search-app/server.js
+++ react-rental-prop-search-app/server.js
@@ -19,29 +19,16 @@ app.use(morgan('tiny'));
 app.use('/api', routes);
 // app.use('/properties', SavedPropertiesRoute);
 
-// DEPLOY START
-app.listen(PORT, console.log(`Server is listening at ${PORT}`));
-
-let uri = "mongodb://localhost/react_prop_search";
-if(process.env.NODE_ENV === 'production') {
-    uri = process.env.MONGODB_URI;
-}
-mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-// DEPLOY END
-
-// mongoose.connect(MONGODB_URI || "mongodb://localhost/react_prop_search", {
-//     useNewUrlParser: true,
-//     useFindAndModify: false,
-//     useUnifiedTopology: true,
-// });
-
-// WORKING CODE PRIOR TO DEPLOY
-// mongoose.connect("mongodb://localhost/react_prop_search", {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true,
-// });
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/react_prop_search", {
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+});
 
 // mongoose listener to verify that mongoose been connected
 mongoose.connection.on("connected", () => {
     console.log("Mongoose is connected!!!");
 });
+
+// DEPLOY START
+app.listen(PORT, console.log(`Server is listening at ${PORT}`));
